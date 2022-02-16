5b91b5023695b4c7339f2637c31231d368c7e22b
masaki2355 <takoyaki2355@icloud.com>
2022-02-16 14:15:19 +0900

    create user_spec.rb

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,9 @@ module Projects
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false
-        view_specs: false
-        helper_specs: false
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
         routing_specs: false
       g.factory_bot false
     end
@@ -29,5 +29,4 @@ end
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-  end
-end
+
